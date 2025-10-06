4d1c755931587e4102fcc3d1b66412652734568b
Parmeet Kumar <parmeetkumarg90.com>
2025-10-06 16:00:16 +0530

    fix login, signup

--- Day 4/NextJs-Authentication/src/components/form/signup-from.tsx
+++ Day 4/NextJs-Authentication/src/components/form/signup-from.tsx
@@ -6,8 +6,8 @@ import CardActions from '@mui/material/CardActions';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { signUpUserInterface } from '@/interfaces/user';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { redirect } from 'next/navigation';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
+import { redirect } from "next/navigation";
 import TextField from '@mui/material/TextField';
 import style from './style.module.css';
 import { logInUserSchema, signUpUserSchema } from '@/schema/authentication';
@@ -21,8 +21,14 @@ import { logInUserInterface } from '@/interfaces/user';
 import { zodResolver } from "@hookform/resolvers/zod"
 
 const SignUpForm = () => {
-    const { register, handleSubmit, reset } = useForm({
+    const { register, handleSubmit, reset, control } = useForm({
         resolver: zodResolver(signUpUserSchema),
+        defaultValues: {
+            email: "",
+            username: "",
+            password: "",
+            confirmPassword: ""
+        }
     });
 
     const users = useSelector((state: RootState) => state.users);
@@ -32,7 +38,7 @@ const SignUpForm = () => {
     useEffect(() => {
         const isValidLogIn = isUserValid(loggedInUser);
         if (isValidLogIn.success) {
-            redirect('/dashboard');
+            redirect(' /dashboard');
         }
     }, []);
 
@@ -42,7 +48,6 @@ const SignUpForm = () => {
         if (isValid.success) {
             const userDetail = users.find((eachUser) => eachUser.email === user.email && eachUser.username === user.username && eachUser.password === user.password);
             if (signUpUserSchema.safeParse(userDetail).success) {
-                dispatch(addCredentials(userDetail!));
                 return { success: true, email: userDetail?.email === user.email, username: userDetail?.username === user.username };
             }
         }
@@ -65,7 +70,7 @@ const SignUpForm = () => {
                 dispatch(addCredentials(data));
                 dispatch(addNewUser(data));
                 enqueueSnackbar("Signup Success");
-                redirect('/dashboard');
+                redirect(' /recipe/create');
             }
         }
         else {
@@ -81,46 +86,79 @@ const SignUpForm = () => {
                         Signup
                     </Typography>
                     <Card className={`${style.card} ${style.input_card}`}>
-                        <TextField
-                            // required
-                            id="filled-basic-username"
-                            {...register("username")}
-                            label="Username"
-                            variant="filled"
-                            className={`${style.w_full}${style.pY}${style.mY}`}
+                        <Controller
+                            name="username"
+                            control={control}
+                            render={({
+                                field, formState: { errors }, fieldState: {
+                                    error,
+                                } }) => {
+                                return <TextField
+                                    {...field}
+                                    helperText={error ? error.message : ""}
+                                    id={`filled-basic-username`}
+                                    label="Username"
+                                    variant="filled"
+                                    error={!!error}
+                                />
+                            }}
                         />
-                        <TextField
-                            // required
-                            id="filled-basic-email"
-                            {...register("email")}
-                            label="Email"
-                            variant="filled"
-                            type='email'
-                            className={`${style.w_full}${style.pY}${style.mY}`}
+                        <Controller
+                            name="email"
+                            control={control}
+                            render={({
+                                field, formState: { errors }, fieldState: {
+                                    error,
+                                } }) => {
+                                return <TextField
+                                    {...field}
+                                    helperText={error ? error.message : ""}
+                                    id={`filled-basic-email`}
+                                    label="Email"
+                                    variant="filled"
+                                    error={!!error}
+                                />
+                            }}
                         />
-                        <TextField
-                            // required
-                            id="filled-basic-password"
-                            {...register("password", { required: true })}
-                            label="Password"
-                            variant="filled"
-                            type='password'
-                            className={`${style.w_full}${style.pY}${style.mY}`}
+                        <Controller
+                            name="password"
+                            control={control}
+                            render={({
+                                field, formState: { errors }, fieldState: {
+                                    error,
+                                } }) => {
+                                return <TextField
+                                    {...field}
+                                    helperText={error ? error.message : ""}
+                                    id={`filled-basic-password`}
+                                    label="Password"
+                                    variant="filled"
+                                    error={!!error}
+                                />
+                            }}
                         />
-                        <TextField
-                            // required
-                            id="filled-basic-confirmPassword"
-                            {...register("confirmPassword", { required: true })}
-                            label="Confirm Password"
-                            variant="filled"
-                            type='password'
-                            className={`${style.w_full}${style.pY}${style.mY}`}
+                        <Controller
+                            name="confirmPassword"
+                            control={control}
+                            render={({
+                                field, formState: { errors }, fieldState: {
+                                    error,
+                                } }) => {
+                                return <TextField
+                                    {...field}
+                                    helperText={error ? error.message : ""}
+                                    id={`filled-basic-confirmPassword`}
+                                    label="Confirm Password"
+                                    variant="filled"
+                                    error={!!error}
+                                />
+                            }}
                         />
                     </Card>
                 </CardContent>
                 <CardActions className={`${style.input_card}`}>
                     <Button size="small" className={`${style.border}`} type='submit'>Create</Button>
-                    <Button size="small" className={`${style.border}`} onClick={() => { redirect('/') }}>Already have an account?</Button>
+                    <Button size="small" className={`${style.border}`} onClick={() => { redirect('/login') }}>Already have an account?</Button>
                 </CardActions>
             </Card>
         </form >
